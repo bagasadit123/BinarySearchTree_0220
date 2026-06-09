e7766ab9212e97d7f625f2d12cdfad4bcaeaadb7
bagasadit123 <bagasadit430@gmail.com>
2026-06-09 11:09:25 +0700

    membuat class binarytree

--- searchtree.cpp
+++ searchtree.cpp
@@ -4,16 +4,27 @@ using namespace std;
 
 class node
 {
-    public:
-        string info;
-        node *leftchild;
-        node *rightchild;
-        
-        //constructor for node class
-        node(string i, node *l, node *r)
-        {
-            info = i;
-            leftchild = l;
-            rightchild = r;
-        }
+public:
+    string info;
+    node *leftchild;
+    node *rightchild;
+
+    //constructor for node class
+    node(string i, node *l, node *r)
+    {
+        info = i;
+        leftchild = l;
+        rightchild = r;
+    }
+};
+
+class binarytree
+{
+public:
+    node *root;
+
+    binarytree()
+    {
+        root = NULL;
+    }
 }
